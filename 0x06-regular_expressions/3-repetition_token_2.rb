2bb930a30d5e7fc370a88a1b9d240dea1b8fc1cf
WairimuMaringa <makena96wairimu@gmail.com>
2022-10-18 04:51:31 -0700

    task three

--- 0x06-regular_expressions/3-repetition_token_2.rb
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
-# The regular expressionthat matches the given cases
-puts ARGV[0].scan(/hbt{0,}n/).join
+# The regular expression that matches the given cases
+puts ARGV[0].scan(/hbt+n/).join

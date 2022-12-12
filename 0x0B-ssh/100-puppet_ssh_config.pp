5a734ff8614590feb484c3064814f0b8cf53932a
WairimuMaringa <makena96wairimu@gmail.com>
2022-12-12 05:40:06 -0800

    advanced

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+# Set up client SSH configuration file
+
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
+}
+
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '   PasswordAuthentication no',
+}
